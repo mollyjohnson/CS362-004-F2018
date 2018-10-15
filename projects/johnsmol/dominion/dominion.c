953853b9fc7ca37169eae29c6fc825b75ab580b7
mollyjohnson <johnsmol@oregonstate.edu>
2018-10-14 21:29:55 -0500

    with functioning village function

--- projects/johnsmol/dominion/dominion.c
+++ projects/johnsmol/dominion/dominion.c
@@ -707,12 +707,18 @@ void baronCard()
 {
 
 }
-
-void villageCard()
-{
-
-}
 */
+void villageCard(int currentPlayer, struct gameState *state, int *handPos)
+{
+	//+1 Card
+    drawCard(currentPlayer, state);
+			
+    //+2 Actions
+    state->numActions = state->numActions + 2;
+			
+    //discard played card from hand
+    discardCard((*handPos), currentPlayer, state, 0);
+}
 
 int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState *state, int handPos, int *bonus)
 {
@@ -867,15 +873,8 @@ int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState
       	return 0;
 		
     case village:
-      //+1 Card
-      drawCard(currentPlayer, state);
-			
-      //+2 Actions
-      state->numActions = state->numActions + 2;
-			
-      //discard played card from hand
-      discardCard(handPos, currentPlayer, state, 0);
-      return 0;
+		villageCard(currentPlayer, state, &handPos);
+        return 0;
 		
     case baron:
       state->numBuys++;//Increase buys by 1!
